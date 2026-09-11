e013fe3a7166d43f24d0cef7535459ab5bdf782a
create-lense <g-ipgp23012796@moe-dl.edu.my>
2026-09-12 07:13:57 +0800

    Upgrade service worker cache version and add icon
    Updated cache version and added icon to cache list.

--- INOVASI/sw.js
+++ INOVASI/sw.js
@@ -1,37 +1,37 @@
-const CACHE_NAME = 'psv-pwa-v2';
+const CACHE_NAME = 'psv-pwa-v4';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
+];
 
-// 1. Apabila Service Worker dipasang, terus ambil alih tanpa tunggu (skipWaiting)
 self.addEventListener('install', (event) => {
   self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll([
-        './',
-        './index.html',
-        './manifest.json'
-      ]);
+      return cache.addAll(urlsToCache);
     })
   );
 });
 
-// 2. Apabila Service Worker diaktifkan, buang cache versi lama (v1)
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cache) => {
-          if (cache !== CACHE_NAME) {
-            return caches.delete(cache);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
+    caches.keys().then((keys) => Promise.all(
+      keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+    )).then(() => self.clients.claim())
   );
 });
 
-// 3. Mengambil data dari rangkaian atau cache
 self.addEventListener('fetch', (event) => {
+  // Guna rangkaian dahulu supaya kemas kini apl terus dipaparkan; jatuh ke cache bila luar talian
   event.respondWith(
-    fetch(event.request).catch(() => caches.match(event.request))
+    fetch(event.request)
+      .then((response) => {
+        const clone = response.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
